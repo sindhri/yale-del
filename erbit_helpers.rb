c696f3e7a90836e4f82e1ca21e74eeac272a025f
Jia Brown <wujia123@yahoo.com>
2012-06-24 19:49:54 -0400

    added html escape, modified data file

--- erbit_helpers.rb
+++ erbit_helpers.rb
@@ -1,10 +1,11 @@
 require "csv"
+require "cgi"
 
 PROJECT_ROOT = File.dirname(__FILE__)
 DATA_DIR = "#{PROJECT_ROOT}/data"
 
 def csv_table(filename)
   CSV.foreach("#{DATA_DIR}/#{filename}", :headers => true) do |row|
-    yield row
+    yield Hash[row.map { |k,v| [k, CGI.escapeHTML(v.to_s)] }]
   end
 end
